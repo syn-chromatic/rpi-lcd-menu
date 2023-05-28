b53815ee4fb65ac1d8d12020dca018f41c9fa5f0
syn-chromatic <synchromatic.github@gmail.com>
2023-05-29 00:09:02 +0300

    update rpi add rotary encoder

---
+++ rpi/rotary.py
@@ -0,0 +1,58 @@
+import lgpio as gp
+from typing import Optional, Literal
+
+
+class RotaryEncoder:
+    def __init__(self, a_pin: int, b_pin: int, sw_pin: int):
+        self.a_pin = a_pin
+        self.b_pin = b_pin
+        self.sw_pin = sw_pin
+        self.handle = gp.gpiochip_open(0)
+        self.a_state = 0
+        self.b_state = 0
+        self.sw_state = 0
+        self.ap_state = 1
+        self.bp_state = 1
+        self.swp_state = 1
+
+    def set_inputs(self):
+        gp.gpio_claim_input(self.handle, self.a_pin, 32)
+        gp.gpio_claim_input(self.handle, self.b_pin, 32)
+        gp.gpio_claim_input(self.handle, self.sw_pin, 32)
+
+    def get_a_state(self):
+        self.a_state = gp.gpio_read(self.handle, self.a_pin)
+        return self.a_state
+
+    def get_b_state(self):
+        self.b_state = gp.gpio_read(self.handle, self.b_pin)
+        return self.b_state
+
+    def get_sw_state(self):
+        self.sw_state = gp.gpio_read(self.handle, self.sw_pin)
+        return self.sw_state
+
+    def get_direction(self) -> Optional[Literal[1, -1]]:
+        self.ap_state = self.a_state
+        self.bp_state = self.b_state
+
+        a_state = self.get_a_state()
+        b_state = self.get_b_state()
+
+        p_state = self.ap_state == 1 and self.bp_state == 1
+
+        if a_state == 1 and b_state == 0 and p_state:
+            return 1
+        elif a_state == 0 and b_state == 1 and p_state:
+            return -1
+
+    def is_pressed(self) -> bool:
+        state = self.get_sw_state()
+        bool_state = False
+        if self.swp_state == 0 and state == 1:
+            bool_state = True
+        self.swp_state = state
+        return bool_state
+
+    def __del__(self):
+        gp.gpiochip_close(self.handle)

e5176768fbfe4a27ad5205b39a8dd8556498ec79
Synchromatic <synchromatic.github@gmail.com>
2023-05-31 12:32:59 +0300

    update lcd i2c

--- rp2040-pico/lcd_i2c.py
+++ rp2040-pico/lcd_i2c.py
@@ -1,6 +1,3 @@
-import gc
-
-
 from lcd_api import LCDAPI
 from x_gpio import GPIOI2C
 
@@ -46,22 +43,18 @@ class I2CLCD(LCDAPI):
         if self.rows > 1:
             cmd |= self.LCD_FUNCTION_2LINES
         self.hal_write_command(cmd)
-        gc.collect()
 
     def hal_write_init_nibble(self, nibble):
         "Put LCD into 4-bit mode."
         byte = ((nibble >> 4) & 0x0F) << SHIFT_DATA
         self.gpio.write_device([byte | MASK_E])
         self.gpio.write_device([byte])
-        gc.collect()
 
     def hal_backlight_on(self):
         self.gpio.write_device([1 << SHIFT_BACKLIGHT])
-        gc.collect()
 
     def hal_backlight_off(self):
         self.gpio.write_device([0])
-        gc.collect()
 
     def hal_write_command(self, cmd):
         byte = (self.backlight << SHIFT_BACKLIGHT) | (((cmd >> 4) & 0x0F) << SHIFT_DATA)
@@ -72,7 +65,6 @@ class I2CLCD(LCDAPI):
         self.gpio.write_device([byte])
         if cmd <= 3:
             self.hal_sleep_ms(5)
-        gc.collect()
 
     def hal_write_data(self, data):
         byte = (
@@ -90,4 +82,3 @@ class I2CLCD(LCDAPI):
         )
         self.gpio.write_device([byte | MASK_E])
         self.gpio.write_device([byte])
-        gc.collect()
